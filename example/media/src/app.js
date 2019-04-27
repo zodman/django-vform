60ebc55c32356ce97e70d44c77accfd422c3053f
zodman <zodman@gmail.com>
2019-04-27 10:48:29 -0500

    vcloak

--- example/media/src/app.js
+++ example/media/src/app.js
@@ -1,9 +1,9 @@
-import {Vue} from 'vue'
-//import { Form, HasError, AlertError } from 'vform'
-import Form from 'vform'
+import  Vue from 'vue'
+import { Form, HasError, AlertError } from 'vform'
+//import Form from 'vform'
  // Register the components
-// Vue.component(HasError.name, HasError)
-// Vue.component(AlertError.name, AlertError)
+ Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 
  new Vue({
    el: '#app',
@@ -22,20 +22,20 @@ import Form from 'vform'
    methods: {
      login () {
        // Since we don't have an actual server, we'll mock the request.
-      // this.mockRequest()
+       this.mockRequest()
 
        // Submit the form via a POST request.
-    //    this.form.post('/login')
-    //      .then(({ data }) => console.log(data))
+       this.form.post('/login')
+          .then(({ data }) => console.log(data))
      },
 
-    //  mockRequest () {
-    //    const mock = new AxiosMockAdapter(axios, { delayResponse: 200 })
+      mockRequest () {
+       const mock = new AxiosMockAdapter(axios, { delayResponse: 200 })
 
-    //    mock.onPost('/login').reply(422, {
-    //      username: ['The username field is required.'],
-    //      password: ['The password field is required.']
-    //    })
-    //  }
+       mock.onPost('/login').reply(422, {
+         username: ['The username field is required.'],
+         password: ['The password field is required.']
+       })
+     }
    }
  })
